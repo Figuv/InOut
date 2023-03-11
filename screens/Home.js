51e35ba1fe688f5cd05bdeda0da1e261d7d03270
Figyuv <david.calvi.18@gmail.com>
2023-03-11 13:59:50 -0400

    App setup

---
+++ screens/Home.js
@@ -0,0 +1,15 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <SafeAreaView>
+        <View>
+            <Text>Home</Text>
+        </View>
+        
+    </SafeAreaView>
+  )
+}
+
+export default Home
---
+++ screens/Register.js
@@ -0,0 +1,14 @@
+import { View, Text, SafeAreaView } from "react-native";
+import React from "react";
+
+const Register = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Register</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Register;
